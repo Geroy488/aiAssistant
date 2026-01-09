dd070c1d364aeb1269544832bd1bf06bf87f5507
Geroy488 <mayecha302@gmail.com>
2026-01-10 01:05:54 +0800

    finish putting the chatbox to homepage

--- server.js
+++ server.js
@@ -1,5 +1,4 @@
 require('rootpath')();
-require('dotenv').config(); // Add this for environment variables
 const express = require('express');
 const app = express();
 const cors = require('cors');
@@ -7,12 +6,14 @@ const errorHandler = require('_middleware/error-handler');
 const path = require('path');
 const bodyParser = require('body-parser')
 const cookieParser = require('cookie-parser');
-
 // Configure CORS once with specific options
+// specify the frontend origin
+// allow cookies and other credentials to be sent
 app.use(cors({origin: 'http://localhost:4200', credentials: true }));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cors());
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
@@ -20,12 +21,10 @@ app.use(cookieParser());
 
 app.use(express.static(path.join(__dirname, 'products')));
 
-// Routes
+app.use(cors({ origin: (origin, callback) => callback(null, true), credentials: true }));
 app.use('/accounts', require('./accounts/account.controller'));
-app.use('/chatbot', require('./chatbot/chatbot.controller')); // Add chatbot route
 app.use('/api-docs', require('./_helpers/swagger'));
 
-// Error handler (must be last)
 app.use(errorHandler);
 
 const port = process.env.NODE_ENV === 'production' ? (process.env.PORT || 80) : 4000;
